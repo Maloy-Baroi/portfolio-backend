77a9283834985526504482ba7bbc44274ada3156
Maloy Baroi (XPL) <44752184+Maloy-Baroi@users.noreply.github.com>
2025-06-04 07:57:03 +0600

    Update serializers.py

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,37 +1,100 @@
 from rest_framework import serializers
-from .models import PersonalInfo, Experience, Education, Skill, Project, Achievement, Contact
+from .models import *
 
-class PersonalInfoSerializer(serializers.ModelSerializer):
+
+class TestSectionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PersonalInfo
+        model = TestSection
         fields = '__all__'
 
+
+class StandardizedTestSerializer(serializers.ModelSerializer):
+    sections = TestSectionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = StandardizedTest
+        fields = '__all__'
+
+
+class TechnicalSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TechnicalSkill
+        fields = '__all__'
+
+
+class TechnicalSkillCategorySerializer(serializers.ModelSerializer):
+    skills = TechnicalSkillSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = TechnicalSkillCategory
+        fields = '__all__'
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = '__all__'
+
+
 class ExperienceSerializer(serializers.ModelSerializer):
+    duration = serializers.ReadOnlyField()
+
     class Meta:
         model = Experience
         fields = '__all__'
 
+
 class EducationSerializer(serializers.ModelSerializer):
+    year_range = serializers.ReadOnlyField()
+    gpa_display = serializers.ReadOnlyField()
+
     class Meta:
         model = Education
         fields = '__all__'
 
-class SkillSerializer(serializers.ModelSerializer):
+
+class ExtraCurricularActivitySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Skill
+        model = ExtraCurricularActivity
         fields = '__all__'
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
 
 class AchievementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Achievement
         fields = '__all__'
 
-class ContactSerializer(serializers.ModelSerializer):
+
+class ReferenceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Contact
+        model = Reference
         fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    experiences = ExperienceSerializer(many=True, read_only=True)
+    educations = EducationSerializer(many=True, read_only=True)
+    test_scores = StandardizedTestSerializer(many=True, read_only=True)
+    skill_categories = TechnicalSkillCategorySerializer(many=True, read_only=True)
+    languages = LanguageSerializer(many=True, read_only=True)
+    activities = ExtraCurricularActivitySerializer(many=True, read_only=True)
+    achievements = AchievementSerializer(many=True, read_only=True)
+    references = ReferenceSerializer(many=True, read_only=True)
+    projects = ProjectSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class ProfileSummarySerializer(serializers.ModelSerializer):
+    """Lighter serializer for listing profiles"""
+    class Meta:
+        model = Profile
+        fields = ['id', 'full_name', 'title', 'email', 'mobile', 'career_objective', 'profile_image']
